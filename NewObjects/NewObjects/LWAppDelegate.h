bb6d161c0fbfc850b12805c3d33f8fe8fbf9d5d4
chaojileiwuhen <465409216@qq.com>
2014-06-10 16:39:11 +0800

    1.0
    项目

---
+++ NewObjects/NewObjects/LWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LWAppDelegate.h
+//  NewObjects
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewObjects/NewObjects/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NewObjects
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LWAppDelegate class]));
+    }
+}
