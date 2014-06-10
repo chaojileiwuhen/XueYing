10ec94858e6d316b87ae9fd0e461e5b95f4fd828
chaojileiwuhen <465409216@qq.com>
2014-06-10 17:30:36 +0800

    add
    add a new object

---
+++ NewObjects/LWFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  LWFirstViewController.h
+//  NewObjects
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWFirstViewController : UIViewController
+
+@end
---
+++ NewObjects/LWFirstViewController.m
@@ -0,0 +1,38 @@
+//
+//  LWFirstViewController.m
+//  NewObjects
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import "LWFirstViewController.h"
+
+@interface LWFirstViewController ()
+
+@end
+
+@implementation LWFirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
